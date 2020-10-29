28d1e464a392e4b873a529ba917b4b22373f0f7a
kazumasa <kazumasa.matsumoto62@gmail.com>
2020-10-30 00:17:28 +0900

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
 sampletest
+testsandbox
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
